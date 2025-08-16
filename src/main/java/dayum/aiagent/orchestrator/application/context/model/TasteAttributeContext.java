3d4ee6afcd286f90ba566bf99a211481095ce50e
slolee <slolee@naver.com>
2025-08-16 22:56:47 +0900

    up

--- src/main/java/dayum/aiagent/orchestrator/application/context/model/TasteAttributeContext.java
+++ src/main/java/dayum/aiagent/orchestrator/application/context/model/TasteAttributeContext.java
@@ -2,4 +2,4 @@ package dayum.aiagent.orchestrator.application.context.model;
 
 import java.util.List;
 
-public record TasteAttributeContext(List<String> dislikedFoods) implements ContextValue {}
+public record TasteAttributeContext(List<String> attributes) implements ContextValue {}
